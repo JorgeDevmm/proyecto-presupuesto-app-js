3b60d05d8126708f1524e8de30ec369b755964b9
JorgeDevmm <jorge29luis10@gmail.com>
2023-12-07 13:54:00 -0500

    Validación del formulario: agregando imprimirtAlerta para ejecutarse dentro de agregarGasto

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ eventListeners();
 
 function eventListeners() {
   document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+  formulario.addEventListener('submit', agregarGasto);
 }
 
 // Classes
@@ -29,11 +31,36 @@ class InterfasUsuario {
     document.querySelector('#total').textContent = presupuesto;
     document.querySelector('#restante').textContent = restante;
   }
+
+  imprimirAlerta(mensaje, tipo) {
+    // Creamos el div html
+    const divMensaje = document.createElement('div');
+
+    // asignamos clases
+    divMensaje.classList.add('text-center', 'alert');
+
+    // validamos el tipo
+    if (tipo === 'error') {
+      divMensaje.classList.add('alert-danger');
+    } else {
+      divMensaje.classList.add('alert-success');
+    }
+    // asignar el cotenido del mensaje al content delhtml
+    divMensaje.textContent = mensaje;
+
+    // Insertamos en el html
+    document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+    // quitar del Html
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
 }
 
 // Instanciar
-const interfasUsuario = new InterfasUsuario();
 let presupuesto;
+const interfasUsuario = new InterfasUsuario();
 
 // Funciones
 
@@ -46,7 +73,7 @@ function preguntarPresupuesto() {
     presupuestoUsuario === '' ||
     presupuestoUsuario === null ||
     isNaN(presupuestoUsuario) ||
-    preguntarPresupuesto <= 0
+    presupuestoUsuario <= 0
   ) {
     //se recargar la pagina, cuando damos cancelar o campo vacio aceptar
     window.location.reload();
@@ -58,4 +85,21 @@ function preguntarPresupuesto() {
 
   // crear html
   interfasUsuario.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+  e.preventDefault();
+
+  const gasto = document.querySelector('#gasto').value;
+  const cantidad = document.querySelector('#cantidad').value;
+
+  if (gasto === '' || cantidad === '') {
+    interfasUsuario.imprimirAlerta('Ambos son obligatorios', 'error');
+    return;
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    interfasUsuario.imprimirAlerta('Cantidad no valida', 'error');
+    return;
+  }
+
+  console.log(`Agregando gasto`);
 }
